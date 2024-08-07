937cc6935e95d0616e11db69b4ad9c98ce4567b5
lavaspoon <lavaspoon@gmail.com>
2024-08-08 00:25:18 +0900

    AuthenticationManager의 구현체 PrividerManager

--- src/main/java/lava/springsecurity5/controller/SampleController.java
+++ src/main/java/lava/springsecurity5/controller/SampleController.java
@@ -1,5 +1,7 @@
 package lava.springsecurity5.controller;
 
+import lava.springsecurity5.service.SampleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.security.Principal;
 
 @Controller
+@RequiredArgsConstructor
 public class SampleController {
+
+    private final SampleService sampleService;
 
     @GetMapping("/")
     public String index(Model model, Principal principal) {
@@ -28,6 +33,7 @@ public class SampleController {
     @GetMapping("/dashboard")
     public String dashboard(Model model, Principal principal) {
         model.addAttribute("message", "Hello Dashboard " + principal.getName());
+        sampleService.dashboard();
         return "dashboard";
     }
 
